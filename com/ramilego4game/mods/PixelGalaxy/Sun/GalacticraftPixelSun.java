b55523c3e1da113cd87f9256b44819a4b8d49806
Rami Sabbagh <ramilego4game@gmail.com>
2014-01-19 10:35:46 +0200

    Create GalacticraftPixelSun.java

---
+++ com/ramilego4game/mods/PixelGalaxy/Sun/GalacticraftPixelSun.java
@@ -0,0 +1,46 @@
+package com.ramilego4game.mods.PixelGalaxy.Sun;
+
+import micdoodle8.mods.galacticraft.api.GalacticraftRegistry;
+import micdoodle8.mods.galacticraft.core.GalacticraftCore;
+
+import com.ramilego4game.mods.PixelGalaxy.Galaxy.GalaxyCommonProxy;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid="GalacticraftPixelSun", name="GalacticraftPixelSun", version="0.0.3", dependencies = "required-after:GalacticraftCore;")
+@NetworkMod(clientSideRequired=true, serverSideRequired=false)
+public class GalacticraftPixelSun {
+	
+	public static GCPixelSun pixelsun;
+	
+        // The instance of your mod that Forge uses.
+        @Instance("GalacticraftPixelSun")
+        public static GalacticraftPixelSun instance;
+       
+        // Says where the client and server 'proxy' code is loaded.
+        public static GalaxyCommonProxy proxy;
+       
+        @EventHandler
+        public void preInit(FMLPreInitializationEvent event) {
+                // Stub Method
+        	GalacticraftPixelSun.pixelsun = new GCPixelSun();
+        	GalacticraftRegistry.registerCelestialBody(GalacticraftPixelSun.pixelsun);
+        }
+       
+        @EventHandler
+        public void load(FMLInitializationEvent event) {
+                //proxy.registerRenderers();
+        }
+       
+        @EventHandler
+        public void postInit(FMLPostInitializationEvent event) {
+                // Stub Method
+        }
+       
+}
