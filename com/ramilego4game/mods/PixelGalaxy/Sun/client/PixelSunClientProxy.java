776033234a22ad55d4df57ee99a36784b32530ef
Rami Sabbagh <ramilego4game@gmail.com>
2014-01-19 10:39:41 +0200

    Create PixelSunClientProxy.java

---
+++ com/ramilego4game/mods/PixelGalaxy/Sun/client/PixelSunClientProxy.java
@@ -0,0 +1,12 @@
+package com.ramilego4game.mods.PixelGalaxy.Sun.client;
+
+import com.ramilego4game.mods.PixelGalaxy.Sun.PixelSunCommonProxy;
+
+
+public class PixelSunClientProxy extends PixelSunCommonProxy{
+	
+	 @Override
+     public void registerRenderers() {
+             // This is for rendering entities and so forth later on
+     }
+}
