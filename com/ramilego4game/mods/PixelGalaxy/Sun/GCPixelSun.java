be7a09c0a5a92ed2f7fb129811d334bf1d76c9fe
Rami Sabbagh <ramilego4game@gmail.com>
2014-01-19 10:37:08 +0200

    Create GCPixelSun.java

---
+++ com/ramilego4game/mods/PixelGalaxy/Sun/GCPixelSun.java
@@ -0,0 +1,53 @@
+package com.ramilego4game.mods.PixelGalaxy.Sun;
+
+import micdoodle8.mods.galacticraft.api.world.IGalaxy;
+import micdoodle8.mods.galacticraft.api.world.IMapObject;
+import micdoodle8.mods.galacticraft.api.world.IPlanet;
+import net.minecraft.world.WorldProvider;
+
+import com.ramilego4game.mods.PixelGalaxy.Galaxy.GalacticraftPixelGalaxy;
+import com.ramilego4game.mods.PixelGalaxy.Sun.client.GCPixelSunMap;
+
+public class GCPixelSun implements IPlanet{
+
+	@Override
+	public String getName() {
+		return "PixelSun";
+	}
+
+	@Override
+	public boolean isReachable() {
+		return false;
+	}
+
+	@Override
+	public IMapObject getMapObject() {
+		return new GCPixelSunMap();
+	}
+
+	@Override
+	public boolean addToList() {
+		return false;
+	}
+
+	@Override
+	public boolean autoRegister() {
+		return false;
+	}
+
+	@Override
+	public Class<? extends WorldProvider> getWorldProvider() {
+		return null;
+	}
+
+	@Override
+	public int getDimensionID() {
+		return -1;
+	}
+
+	@Override
+	public IGalaxy getParentGalaxy() {
+		return GalacticraftPixelGalaxy.PixelGalaxy;
+	}
+
+}
