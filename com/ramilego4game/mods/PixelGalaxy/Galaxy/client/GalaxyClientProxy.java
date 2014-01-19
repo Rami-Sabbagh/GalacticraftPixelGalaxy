e5ea5bf7bad124205f6953974af592fa9b7f2abc
Rami Sabbagh <ramilego4game@gmail.com>
2014-01-19 10:34:55 +0200

    Create GalaxyClientProxy.java

---
+++ com/ramilego4game/mods/PixelGalaxy/Galaxy/client/GalaxyClientProxy.java
@@ -0,0 +1,12 @@
+package com.ramilego4game.mods.PixelGalaxy.Galaxy.client;
+
+import com.ramilego4game.mods.PixelGalaxy.Galaxy.GalaxyCommonProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class GalaxyClientProxy extends GalaxyCommonProxy{
+	
+	 @Override
+     public void registerRenderers() {
+             // This is for rendering entities and so forth later on
+     }
+}
