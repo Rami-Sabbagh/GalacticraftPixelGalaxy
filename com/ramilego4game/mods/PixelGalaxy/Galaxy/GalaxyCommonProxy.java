bdc8047f4fb90e33edf9fef0fac11a6d9eff8e55
Rami Sabbagh <ramilego4game@gmail.com>
2014-01-19 10:34:01 +0200

    Create GalaxyCommonProxy.java

---
+++ com/ramilego4game/mods/PixelGalaxy/Galaxy/GalaxyCommonProxy.java
@@ -0,0 +1,10 @@
+package com.ramilego4game.mods.PixelGalaxy.Galaxy;
+
+public class GalaxyCommonProxy {
+	
+	// Client stuff
+    public void registerRenderers() {
+            // Nothing here as the server doesn't render graphics or entities!
+    }
+	
+}
