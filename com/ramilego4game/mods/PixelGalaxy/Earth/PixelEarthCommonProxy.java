e0197233202dc8ca1b4f0f2285bc095fe89566d7
Rami Sabbagh <ramilego4game@gmail.com>
2014-01-19 10:41:17 +0200

    Create PixelEarthCommonProxy.java

---
+++ com/ramilego4game/mods/PixelGalaxy/Earth/PixelEarthCommonProxy.java
@@ -0,0 +1,8 @@
+package com.ramilego4game.mods.PixelGalaxy.Earth;
+
+public class PixelEarthCommonProxy {
+	// Client stuff
+    public void registerRenderers() {
+            // Nothing here as the server doesn't render graphics or entities!
+    }
+}
