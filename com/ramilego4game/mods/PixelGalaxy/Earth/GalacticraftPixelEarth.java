8d3c55908bf253af1a98c82d69289133eeaa9e5e
RamiLego4Game <ramilego4game@gmail.com>
2014-05-20 21:38:01 +0300

    Cleared GCAPI

--- com/ramilego4game/mods/PixelGalaxy/Earth/GalacticraftPixelEarth.java
+++ com/ramilego4game/mods/PixelGalaxy/Earth/GalacticraftPixelEarth.java
@@ -1,6 +1,5 @@
 package com.ramilego4game.mods.PixelGalaxy.Earth;
 
-import micdoodle8.mods.galacticraft.api.GalacticraftRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
@@ -23,21 +22,9 @@ import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid="GalacticraftPixelEarthID", name="GalacticraftPixelEarth", version="0.0.7", dependencies = "required-after:GalacticraftCore;")
+@Mod(modid="GalacticraftPixelEarthID", name="GalacticraftPixelEarth", version="0.0.7")
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class GalacticraftPixelEarth {
-	
-	//Config Varibles
-	public static int dimearth = -45;
-	public static int pemainid = 150;
-	public static int pgrassid = 200;
-	public static int pdirtid = 201;
-	public static int pstoneid = 202;
-	
-	//Our PixelEarth
-	public static GCPixelEarth pixelearth;
-	
-	
 	
 	//Creative Tab
 	public static CreativeTabs tabPEarth = new CreativeTabs("tabPEarth") {
@@ -51,7 +38,7 @@ public class GalacticraftPixelEarth {
 		public final static Block pixelDirt = new GreenBlock(201, Material.ground).setHardness(0.6f).setStepSound(Block.soundGravelFootstep).setUnlocalizedName("pixeldirt").setTextureName("galacticraftpixel:pixeldirt");
 		public final static Block pixelStone = new NormBlock(202, Material.rock).setHardness(0.6f).setStepSound(Block.soundStoneFootstep).setUnlocalizedName("pixelstone").setTextureName("galacticraftpixel:pixelstone");
 		
-		public static BiomeGenBase PEBiome = new BiomeGenPEarth(pemainid).setBiomeName("PEMain").setMinMaxHeight(-1F, 1F);
+		public static BiomeGenBase PEBiome = new BiomeGenPEarth(150).setBiomeName("PEMain").setMinMaxHeight(-1F, 1F);
 	
 		
 		
@@ -66,19 +53,6 @@ public class GalacticraftPixelEarth {
         @EventHandler
         public void preInit(FMLPreInitializationEvent event) {
                 // Stub Method
-        	Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-        	
-        	config.load();
-        	
-        	/*pemainid = config.getBlock(Configuration.CATEGORY_GENERAL,"MainPixelEarthBiomeID", 150).getInt();
-        	dimearth = config.getBlock(Configuration.CATEGORY_GENERAL,"PixelEarthDimID", -45).getInt();
-        	pgrassid = config.getBlock(Configuration.CATEGORY_BLOCK,"PixelEarthGrassID", 200).getInt();
-        	pdirtid = config.getBlock(Configuration.CATEGORY_BLOCK,"PixelDirtGrassID", 201).getInt();
-        	pstoneid = config.getBlock(Configuration.CATEGORY_BLOCK,"PixelStoneGrassID", 202).getInt();*/
-        	
-        	config.save();
-        	
-        	
         }
        
         @EventHandler
@@ -93,8 +67,7 @@ public class GalacticraftPixelEarth {
         	GameRegistry.registerBlock(pixelStone, "pixelstone");
         	LanguageRegistry.addName(pixelStone, "Pixel Stone");
         	
-        	GalacticraftRegistry.registerCelestialBody(new GCPixelEarth());
-        	//GalacticraftRegistry.registerTeleportType(GCPixelEarthWorldProvider.class, new GCPixelEarthTeleportType());
+        	GameRegistry.addBiome(PEBiome);
         }
        
         @EventHandler
