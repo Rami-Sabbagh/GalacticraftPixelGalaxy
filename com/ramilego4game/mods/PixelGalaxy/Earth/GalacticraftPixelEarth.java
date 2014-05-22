7a9342ad59c3c2c9a6ef8e8289d6fb625fd09f1e
RamiLego4Game <ramilego4game@gmail.com>
2014-05-22 21:23:43 +0300

    Working At 0.0.8 Currently Dev 0.0.75.3

--- com/ramilego4game/mods/PixelGalaxy/Earth/GalacticraftPixelEarth.java
+++ com/ramilego4game/mods/PixelGalaxy/Earth/GalacticraftPixelEarth.java
@@ -4,6 +4,7 @@ import micdoodle8.mods.galacticraft.api.GalacticraftRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.EnumToolMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -17,12 +18,14 @@ import com.ramilego4game.mods.PixelGalaxy.Earth.blocks.NormBlock;
 import com.ramilego4game.mods.PixelGalaxy.Earth.dimension.GCPixelEarthTeleportType;
 import com.ramilego4game.mods.PixelGalaxy.Earth.dimension.GCPixelEarthWorldProvider;
 import com.ramilego4game.mods.PixelGalaxy.Earth.items.NormItem;
+import com.ramilego4game.mods.PixelGalaxy.Earth.items.PixelizerArmor;
 import com.ramilego4game.mods.PixelGalaxy.Earth.items.axeItem;
 import com.ramilego4game.mods.PixelGalaxy.Earth.items.hoeItem;
 import com.ramilego4game.mods.PixelGalaxy.Earth.items.pickaxeItem;
 import com.ramilego4game.mods.PixelGalaxy.Earth.items.shovelItem;
 import com.ramilego4game.mods.PixelGalaxy.Earth.items.swordItem;
 
+import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -69,6 +72,14 @@ public class GalacticraftPixelEarth {
 		public static Item PixelizerHoe = new hoeItem(4504, PEarthTool).setUnlocalizedName("pixelizerhoe").setTextureName("galacticraftpixel:pixelizer_hoe");
 		public static Item PixelizerSword = new swordItem(4505, PEarthTool).setUnlocalizedName("pixelizersword").setTextureName("galacticraftpixel:pixelizer_sword");
 		
+		//Armor
+		public static EnumArmorMaterial PixelizerArmor = EnumHelper.addArmorMaterial("PixelizerArmor", 8, new int[] { 6, 14, 12, 6 }, 30);
+		
+		public static Item PixelizerHelmet = new PixelizerArmor(4506, PixelizerArmor, 5, 0).setUnlocalizedName("pixelizer_helmet").setTextureName("galacticraftpixel:pixelizer_helmet");
+		public static Item PixelizerChestplate = new PixelizerArmor(4507, PixelizerArmor, 5, 1).setUnlocalizedName("pixelizer_chestplate").setTextureName("galacticraftpixel:pixelizer_chestplate");
+		public static Item PixelizerLeggings = new PixelizerArmor(4508, PixelizerArmor, 5, 2).setUnlocalizedName("pixelizer_leggings").setTextureName("galacticraftpixel:pixelizer_leggings");
+		public static Item PixelizerBoots = new PixelizerArmor(4509, PixelizerArmor, 5, 3).setUnlocalizedName("pixelizer_boots").setTextureName("galacticraftpixel:pixelizer_boots");
+		
         // The instance of your mod that Forge uses.
         @Instance("GalacticraftPixelEarthID")
         public static GalacticraftPixelEarth instance;
@@ -101,6 +112,8 @@ public class GalacticraftPixelEarth {
         	
         	LanguageRegistry.instance().addStringLocalization("itemGroup.tabPEarth", "en_US", "PixelEarth");
         	
+        	RenderingRegistry.addNewArmourRendererPrefix("PEarthArmor");
+        	
         	GameRegistry.registerBlock(pixelBedrock, "pixelbedrock");
         	LanguageRegistry.addName(pixelBedrock, "Pixel Bedrock");
         	GameRegistry.registerBlock(pixelGrass, "pixelgrass");
@@ -123,6 +136,11 @@ public class GalacticraftPixelEarth {
         	LanguageRegistry.addName(PixelizerHoe, "Pixelizer Hoe");
         	LanguageRegistry.addName(PixelizerSword, "Pixelizer Sword");
         	
+        	LanguageRegistry.addName(PixelizerHelmet, "Pixelizer Helmet");
+        	LanguageRegistry.addName(PixelizerChestplate, "Pixelizer Chestplate");
+        	LanguageRegistry.addName(PixelizerLeggings, "Pixelizer Leggings");
+        	LanguageRegistry.addName(PixelizerBoots, "Pixelizer Boots");
+        	
         	
         	LanguageRegistry.instance().addStringLocalization("gui.choosePlanet.desc.pixelearth", "en_US", "Habitable#No Oxygen#Gravity: 100%");
 			LanguageRegistry.instance().addStringLocalization("dimension.PixelEarth.name", "en_US", "PixelEarth");
@@ -134,25 +152,16 @@ public class GalacticraftPixelEarth {
 			GameRegistry.addSmelting(pixelizerOre.blockID, pixelizerstack, 3.5f);
 			
 			//Crafting Recipes
-			GameRegistry.addRecipe(new ItemStack(PixelizerPickaxe,1), new Object[]{
-				"TTT"," Y "," Y ",'T',PixelizerIngot,'Y',Item.stick
-				});
-
-
-			GameRegistry.addRecipe(new ItemStack(PixelizerSword,1), new Object[]{
-				" T "," T "," Y ",'T',PixelizerIngot,'Y',Item.stick
-				});
-
-
-			GameRegistry.addRecipe(new ItemStack(PixelizerAxe,1), new Object[]{
-				"TT ","TY "," Y ",'T',PixelizerIngot,'Y',Item.stick
-				});
-			GameRegistry.addRecipe(new ItemStack(PixelizerHoe,1), new Object[]{
-				"TT "," Y "," Y ",'T',PixelizerIngot,'Y',Item.stick
-				});
-			GameRegistry.addRecipe(new ItemStack(PixelizerShovel,1), new Object[]{
-				" T "," Y "," Y ",'T',PixelizerIngot,'Y',Item.stick
-				});
+			GameRegistry.addRecipe(new ItemStack(PixelizerPickaxe,1), new Object[]{"TTT"," Y "," Y ",'T',PixelizerIngot,'Y',Item.stick});
+			GameRegistry.addRecipe(new ItemStack(PixelizerSword,1), new Object[]{" T "," T "," Y ",'T',PixelizerIngot,'Y',Item.stick});
+			GameRegistry.addRecipe(new ItemStack(PixelizerAxe,1), new Object[]{"TT ","TY "," Y ",'T',PixelizerIngot,'Y',Item.stick});
+			GameRegistry.addRecipe(new ItemStack(PixelizerHoe,1), new Object[]{"TT "," Y "," Y ",'T',PixelizerIngot,'Y',Item.stick});
+			GameRegistry.addRecipe(new ItemStack(PixelizerShovel,1), new Object[]{" T "," Y "," Y ",'T',PixelizerIngot,'Y',Item.stick});
+			
+			GameRegistry.addRecipe(new ItemStack(PixelizerHelmet,1), new Object[]{"TTT","T T","   ",'T',PixelizerIngot});
+			GameRegistry.addRecipe(new ItemStack(PixelizerChestplate,1), new Object[]{"T T","TTT","TTT",'T',PixelizerIngot});
+			GameRegistry.addRecipe(new ItemStack(PixelizerLeggings,1), new Object[]{"TTT","T T","T T",'T',PixelizerIngot});
+			GameRegistry.addRecipe(new ItemStack(PixelizerBoots,1), new Object[]{"T T","T T","   ",'T',PixelizerIngot});
         }
        
         @EventHandler
